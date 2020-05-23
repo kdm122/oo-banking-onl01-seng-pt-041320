706c85941e770b4f19e348692ced03c0fb508a54
Kyle McLean <kyledmclean@gmail.com>
2020-05-23 04:39:57 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,24 @@
 class BankAccount
+  attr_accessor :name, :balance, :status
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(cash)
+    @balance += cash
+  end
+
+  def display_balance
+    @balance
+  end
+
+  def valid?
+    if @status == 'open' && @balance > 0
+      true
+    end
+  end
 
 end
