f9a310073ee41bb6511c1781ae754d245d5ba9a4
Kyle McLean <kyledmclean@gmail.com>
2020-05-23 04:42:42 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -13,11 +13,15 @@ class BankAccount
   end
 
   def display_balance
-    @balance
+    puts "Your balance is $#{@balance}."
   end
 
   def valid?
     @status == 'open' && @balance > 0 ? true : false
+  end
+
+  def close_account
+    @status = "closed"
   end
 
 end
